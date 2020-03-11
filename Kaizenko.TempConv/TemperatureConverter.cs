8eea9e8d596f5a457a48ba0c1f870d04a6ab4f09
jbaker <jpbaker@anl.gov>
2020-03-11 13:04:42 -0500

    Initial

---
+++ Kaizenko.TempConv/TemperatureConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaizenko.TempConv
+{
+    public class TemperatureConverter
+    {
+        public double ConvertCtoF(double tempInC)
+        {
+            double tempInF = tempInC * 9 / 5 + 32;
+            return tempInF;
+        }
+    }
+}
